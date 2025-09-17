47a9e77abc2b38ff296d896437adfae478778d98
Manvi Mudgal <manvimudgal99@gmail.com>
2025-09-17 19:14:38 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import Body from './Body'
 import Header from './Header'
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Showdetails from './Showdetails'
 
 const App = () => {
    const [show,setShow] = useState([])
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header show={show} />
       <Routes>
         <Route path='/' element={<Body  show={show} setShow={setShow}/>} />
         <Route path='/show/:id' element={<Showdetails show={show} setShow={setShow} />} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   )
 }
